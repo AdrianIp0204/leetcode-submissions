2b2ca0c220f7dd9ca093d04fb1f4f6ea73e4bf7d
Hoi Hin Adrian Ip <dreamresearcher0204@gmail.com>
2026-06-04 14:13:55 +0800

    Auto-sync LeetCode submissions 2026-06-04T06:13Z

---
+++ submissions/1021-remove-outermost-parentheses/README.md
@@ -0,0 +1,17 @@
+# Remove Outermost Parentheses
+
+- LeetCode: https://leetcode.com/problems/remove-outermost-parentheses/
+- Language: python3
+- Exported at: 2026-06-04T06:13:34.264Z
+- Submission status seen by extension: Accepted
+- Submitted at: 2026-06-04T06:13:30.000Z
+- Submission ID: 2021947957
+
+## Key Idea
+
+TODO
+
+## Complexity
+
+- Time: TODO
+- Space: TODO
---
+++ submissions/1021-remove-outermost-parentheses/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        res, opened = [], 0
+        for c in s:
+            if c == "(" and opened > 0:
+                res.append(c)
+            elif c == ")" and opened > 1:
+                res.append(c)
+            if c == "(":
+                opened += 1
+            else:
+                opened -=1
+        return "".join(res)
