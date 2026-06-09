0efb29bde0149de808b85b16699a2b22054accd5
Hoi Hin Adrian Ip <dreamresearcher0204@gmail.com>
2026-06-09 19:42:40 +0800

    Auto-sync LeetCode submissions 2026-06-09T11:42Z

--- submissions/0268-missing-number/solution.py
+++ submissions/0268-missing-number/solution.py
@@ -1,9 +1,5 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums) + 1
-        nl = [-1] * n
-        for i in nums:
-            nl[i] = i
-        for i in range(n):
-            if nl[i] == -1:
+    def missingNumber(self, n: List[int]) -> int:
+        for i in range(len(n)+1):
+            if i not in n:
                 return i
