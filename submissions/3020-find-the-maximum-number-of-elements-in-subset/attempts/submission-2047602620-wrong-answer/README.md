e0d652910cb2d9409495946695fd5a5324b5f84d
Hoi Hin Adrian Ip <dreamresearcher0204@gmail.com>
2026-06-27 16:07:48 +0800

    Auto-sync LeetCode submissions 2026-06-27T08:07Z

---
+++ submissions/3020-find-the-maximum-number-of-elements-in-subset/attempts/submission-2047602620-wrong-answer/README.md
@@ -0,0 +1,29 @@
+# Find the Maximum Number of Elements in Subset
+
+- LeetCode: https://leetcode.com/problems/find-the-maximum-number-of-elements-in-subset/
+- Language: python3
+- Exported at: 2026-06-27T08:07:38.811Z
+- Submission status seen by extension: Wrong Answer
+- Difficulty: Medium
+- Tags: Array, Hash Table, Enumeration
+- Runtime: 102
+- Memory: 31512000
+- Submitted at: 2026-06-27T08:07:34.000Z
+- Submission ID: 2047602620
+
+## Pattern
+
+TODO
+
+## Key Idea
+
+TODO
+
+## Mistake / Edge Case
+
+TODO
+
+## Complexity
+
+- Time: TODO
+- Space: TODO
---
+++ submissions/3020-find-the-maximum-number-of-elements-in-subset/attempts/submission-2047602620-wrong-answer/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        nap = Counter(nums)
+        ans1 = 0
+        ans2 = 1
+        if 1 in nap:
+            if nap[1] % 2:
+                ans1 = nap[1]
+            del nap[1]
+        
+        for x in nap:
+            tmp = 1
+            p = 2
+            for _ in range(30):
+                n = pow(x, p)
+                if n in nap and nap[x] >= 2 and nap[n] >= 1:
+                    tmp += 2
+                    p *= 2
+                else:
+                    break
+            ans2 = max(ans2, tmp)
+        return max(ans1, ans2)
