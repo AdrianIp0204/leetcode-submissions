1f656781190d9f5bfe339bd8dc0f39a0a2868791
Hoi Hin Adrian Ip <dreamresearcher0204@gmail.com>
2026-06-29 16:49:21 +0800

    Auto-sync LeetCode submissions 2026-06-29T08:49Z

---
+++ submissions/0008-string-to-integer-atoi/attempts/submission-2049823548-runtime-error/README.md
@@ -0,0 +1,29 @@
+# String to Integer (atoi)
+
+- LeetCode: https://leetcode.com/problems/string-to-integer-atoi/
+- Language: python3
+- Exported at: 2026-06-29T08:48:57.437Z
+- Submission status seen by extension: Runtime Error
+- Difficulty: Medium
+- Tags: String
+- Runtime: 33
+- Memory: 19428000
+- Submitted at: 2026-06-29T08:48:50.000Z
+- Submission ID: 2049823548
+
+## Pattern
+
+TODO
+
+## Key Idea
+
+TODO
+
+## Mistake / Edge Case
+
+TODO
+
+## Complexity
+
+- Time: TODO
+- Space: TODO
---
+++ submissions/0008-string-to-integer-atoi/attempts/submission-2049823548-runtime-error/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        neg = False
+        s = s.strip()
+        res = 0
+        if s[0] == '-':
+            s = s[1:]
+            neg = True
+        for n in s:
+            if n.isnumeric():
+                res = res * 10 + int(n)
+            else:
+                break
+        if neg:
+            res *= -1
+            if res < -2147483648:
+                res = -2147483648
+        elif res > 2147483647:
+            res = 2147483647
+        return res
