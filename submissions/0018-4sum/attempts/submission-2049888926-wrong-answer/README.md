4fb1270385001bf4ff9aae2e81b6fd53ae073439
Hoi Hin Adrian Ip <dreamresearcher0204@gmail.com>
2026-06-29 17:48:22 +0800

    Auto-sync LeetCode submissions 2026-06-29T09:48Z

---
+++ submissions/0018-4sum/attempts/submission-2049888926-wrong-answer/README.md
@@ -0,0 +1,29 @@
+# 4Sum
+
+- LeetCode: https://leetcode.com/problems/4sum/
+- Language: python3
+- Exported at: 2026-06-29T09:48:09.911Z
+- Submission status seen by extension: Wrong Answer
+- Difficulty: Medium
+- Tags: Array, Two Pointers, Sorting
+- Runtime: 10422
+- Memory: 19324000
+- Submitted at: 2026-06-29T09:47:54.000Z
+- Submission ID: 2049888926
+
+## Pattern
+
+TODO
+
+## Key Idea
+
+TODO
+
+## Mistake / Edge Case
+
+TODO
+
+## Complexity
+
+- Time: TODO
+- Space: TODO
---
+++ submissions/0018-4sum/attempts/submission-2049888926-wrong-answer/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if len(nums) < 4:
+            return []
+        res = []
+        n = len(nums)
+        for i in range(n):
+            a = nums[i]
+            for j in range(i + 1, n):
+                b = nums[j]
+                for k in range(j + 1, n):
+                    c = nums[k]
+                    for w in range(k + 1, n):
+                        tmp = [a, b, c, nums[w]]
+                        tot = sum(tmp)
+                        if sum(tmp) == target:
+                            if tmp not in res:
+                                res.append([a, b, c, nums[w]])
+        return res
